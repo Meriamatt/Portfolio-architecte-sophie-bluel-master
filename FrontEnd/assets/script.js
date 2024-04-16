5c6059829bd368c1c783668e76bfc8e872a99926
Meriam Attaiki <mer.attaiki@gmail.com>
2024-04-16 20:59:20 +0200

    first commit

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,88 @@
+let works = [];
+let categories = [];
+const reponse = fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((json) => {
+    works = json;
+    clearGalery();
+    displayGallery(works);
+  });
+  const response = fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((json) => {
+      categories = json;
+      addFilter(categories);
+      console.log(categories);
+      });
+let gallery = document.querySelector(".gallery");
+function clearGalery() {
+  gallery.innerHTML = "";
+}
+
+
+//function to create button called filterName
+function createButton(filterName){
+    let filter = document.getElementById("filterContainer");
+    let section = document.getElementById("portfolio");
+    console.log(filter);
+    let button = document.createElement("button");
+    button.innerText = filterName;
+    button.className = "filter";
+      filter.appendChild(button);
+      section.appendChild(filter);
+      section.insertBefore(filter, gallery);
+      
+}
+
+
+
+ let filterSelected = document.getElementsByClassName ("filter");
+  let filterActive = document.getElementsByClassName ("active");
+function addFilter(categories) {
+
+ 
+ for (let i = 0; i < categories.length; i++){
+// instance of filterName with the value of categories name from the table categories
+ createButton(categories[i].name);
+ console.log(filterSelected);
+       filterSelected[i+1].addEventListener("click", function() {
+             console.log("click", categories[i].name);
+             let categorieName = categories[i].name;
+             filterActive[0].classList.remove ("active");
+             filterSelected[i+1].classList.add ("active");
+             clearGalery();
+             let filteredCategorie = works.filter((works) => {
+                 return works.category.name == categorieName;
+             });
+             console.log(filteredCategorie);
+             displayGallery(filteredCategorie);
+
+             });
+ }
+
+  
+}
+ let firstFilterSelected = document.getElementById ("selected");
+ firstFilterSelected.addEventListener("click", function() {
+  
+              console.log("click", firstFilterSelected);
+              filterActive[0].classList.remove ("active");
+              firstFilterSelected.classList.add("active");
+              console.log(filterActive);
+              clearGalery();
+              displayGallery(works);
+              });
+      
+   
+function displayGallery(works) {
+  for (let i = 0; i < works.length; i++) {
+    let image = document.createElement("img");
+    let figcaption = document.createElement("figcaption");
+    image.src = works[i].imageUrl;
+    figcaption = works[i].title;
+    let div = document.createElement("div");
+    gallery.appendChild(div);
+    div.append(image);
+    div.append(figcaption);
+  }
+}
